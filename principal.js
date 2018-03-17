ea67cd637859783e89a2c1f4c955a72a621190ee
Renato Martins <renatomartsh@gmail.com>
2018-03-17 16:44:57 -0300

    Banner ao abrir pagina

--- principal.js
+++ principal.js
@@ -5,4 +5,4 @@ function trocaBanner() {
   bannerAtual = (bannerAtual + 1) % 2;
   document.querySelector('h2#mensagem').textContent =  banners[bannerAtual];
 }
-setInterval(trocaBanner, 2000);
+setInterval(trocaBanner, 1500);
